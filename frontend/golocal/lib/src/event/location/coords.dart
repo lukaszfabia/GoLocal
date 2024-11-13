834756a5882c475cd82b33988a029c045a38f917
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-11-13 12:26:01 +0100

    renamed, added serialization methods

--- frontend/golocal/lib/src/event/location/coords.dart
+++ frontend/golocal/lib/src/event/location/coords.dart
@@ -1,12 +1,25 @@
 import 'package:golocal/src/shared/model_base.dart';
 
-class CoordsDTO extends Model {
+class Coords extends Model {
   double latitude;
   double longitude;
 
-  CoordsDTO({
+  Coords({
     required super.id,
     required this.latitude,
     required this.longitude,
   });
+
+  Coords.fromJson(super.json)
+      : latitude = json['latitude'],
+        longitude = json['longitude'],
+        super.fromJson();
+
+  @override
+  Map<String, dynamic> toJson() {
+    final data = super.toJson();
+    data['latitude'] = latitude;
+    data['longitude'] = longitude;
+    return data;
+  }
 }
--- frontend/golocal/lib/src/event/location/location.dart
+++ frontend/golocal/lib/src/event/location/location.dart
@@ -2,19 +2,43 @@ import 'package:golocal/src/event/location/address.dart';
 import 'package:golocal/src/event/location/coords.dart';
 import 'package:golocal/src/shared/model_base.dart';
 
-class LocationDTO extends Model {
+class Location extends Model {
   String city;
   String country;
-  String zip;
+  String? zip;
 
-  CoordsDTO coords;
-  AddressDTO address;
-  LocationDTO({
+  Coords? coords;
+  Address? address;
+  Location({
     required super.id,
     required this.city,
     required this.country,
-    required this.zip,
-    required this.coords,
-    required this.address,
+    this.zip,
+    this.coords,
+    this.address,
   });
+
+  Location.fromJson(super.json)
+      : city = json['city'],
+        country = json['country'],
+        zip = json['zip'],
+        coords = Coords.fromJson(json['coords']),
+        address = Address.fromJson(json['address']),
+        super.fromJson();
+
+  @override
+  Map<String, dynamic> toJson() {
+    final data = super.toJson();
+    data['city'] = city;
+    data['country'] = country;
+    data['zip'] = zip ?? '';
+    data['coords'] = coords?.toJson() ?? {};
+    data['address'] = address?.toJson() ?? {};
+    return data;
+  }
+
+  @override
+  String toString() {
+    return 'Location{city: $city, country: $country, zip: $zip, coords: $coords, address: $address}';
+  }
 }
