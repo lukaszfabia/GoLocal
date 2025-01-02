ebed8956e4d0921130ca3f9a1ff26d9fb4996c59
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-01-02 22:51:31 +0100

    fixed something

--- mobile-app/lib/src/routing/router.dart
+++ mobile-app/lib/src/routing/router.dart
@@ -4,6 +4,8 @@ import 'package:go_router/go_router.dart';
 import 'package:golocal/src/app.dart';
 import 'package:golocal/src/auth/bloc/auth_bloc.dart';
 import 'package:golocal/src/auth/ui/auth_screen.dart';
+import 'package:golocal/src/event/domain/event.dart';
+import 'package:golocal/src/event/ui/event_detail_page.dart';
 import 'package:golocal/src/event/ui/events_map.dart';
 import 'package:golocal/src/event/ui/events_view_page.dart';
 import 'package:golocal/src/preference_survey/ui/preference_survey_page.dart';
@@ -20,15 +22,22 @@ abstract class AppRouter {
   static final GlobalKey<NavigatorState> rootNavigatorKey =
       GlobalKey<NavigatorState>(debugLabel: "root");
   static final GlobalKey<NavigatorState> shellNavigatorKeyHome =
-      GlobalKey<NavigatorState>();
+      GlobalKey<NavigatorState>(debugLabel: 'home');
   static final GlobalKey<NavigatorState> shellNavigatorKeyProfile =
-      GlobalKey<NavigatorState>();
+      GlobalKey<NavigatorState>(debugLabel: 'profile');
 
   static GoRouter router(AuthBloc authBloc) {
     _router ??= GoRouter(
+      debugLogDiagnostics: true,
       navigatorKey: rootNavigatorKey,
       initialLocation: AppRoute.splash.path,
       routes: [
+        GoRoute(
+            path: '/',
+            redirect: (context, state) {
+              bool isLoggedIn = authBloc.state is Authenticated;
+              return isLoggedIn ? AppRoute.events.path : AppRoute.auth.path;
+            }),
         GoRoute(
           path: AppRoute.auth.path,
           name: AppRoute.auth.name,
@@ -42,8 +51,9 @@ abstract class AppRouter {
         // shell for main screens with navbar
         StatefulShellRoute.indexedStack(
           parentNavigatorKey: rootNavigatorKey,
-          builder: (context, state, navigationShell) =>
-              ScaffoldShell(navigationShell: navigationShell),
+          builder: (context, state, navigationShell) {
+            return ScaffoldShell(navigationShell: navigationShell, title: null);
+          },
           branches: [
             // []
             StatefulShellBranch(
@@ -56,17 +66,29 @@ abstract class AppRouter {
                 ),
               ],
             ),
+            // events
             StatefulShellBranch(
+              navigatorKey: shellNavigatorKeyHome,
               routes: [
                 GoRoute(
-                  path: AppRoute.home.path,
+                  path: AppRoute.events.path,
                   builder: (context, state) {
                     return const EventsViewPage();
                   },
+                  routes: [
+                    GoRoute(
+                      path: AppRoute.eventDetail.path,
+                      builder: (context, state) {
+                        final event = state.extra as Event;
+                        return EventDetailPage(event: event);
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
             StatefulShellBranch(
+              navigatorKey: shellNavigatorKeyProfile,
               routes: [
                 GoRoute(
                     path: AppRoute.profile.path,
@@ -91,10 +113,9 @@ abstract class AppRouter {
             state.matchedLocation == AppRoute.splash.path);
         final AuthState authState = authBloc.state;
         final bool isLoggedIn = authState is Authenticated;
-        print(authState.toString());
-        print(state.matchedLocation);
-        if (authState is Authenticated && isLoggingIn) {
-          return AppRoute.home.path;
+
+        if (isLoggedIn && isLoggingIn) {
+          return AppRoute.events.path;
         } else if (authState is Unathenticated) {
           return AppRoute.auth.path;
         }
@@ -110,14 +131,18 @@ abstract class AppRouter {
 // Much easier to maintain and add new routes
 enum AppRoute {
   splash(path: '/splash', name: 'splash'),
-  home(path: '/home', name: 'home'),
+
+  events(path: '/events', name: 'events', title: "Events"),
+  eventDetail(path: '/:id', name: 'eventDetail'),
+
   profile(path: '/profile', name: 'profile'),
-  survey(path: '/profile/survey', name: 'survey'),
+  survey(path: '/survey', name: 'survey', title: "Preference Survey"),
   map(path: '/map', name: 'map'),
   auth(path: '/auth', name: 'auth');
 
-  const AppRoute({required this.path, required this.name});
+  const AppRoute({required this.path, required this.name, this.title});
 
   final String path;
   final String name;
+  final String? title;
 }
--- mobile-app/lib/src/shared/scaffold_shell.dart
+++ mobile-app/lib/src/shared/scaffold_shell.dart
@@ -4,17 +4,23 @@ import 'package:golocal/src/event/ui/events_view_page.dart';
 import 'package:golocal/src/user/ui/profile_page.dart';
 import 'package:golocal/src/routing/router.dart';
 
-class ScaffoldShell extends StatelessWidget {
+class ScaffoldShell extends StatefulWidget {
   final StatefulNavigationShell navigationShell;
-  const ScaffoldShell({Key? key, required this.navigationShell})
+  final String? title;
+  const ScaffoldShell({Key? key, required this.navigationShell, this.title})
       : super(key: key ?? const ValueKey("ScaffoldShell"));
 
   @override
+  State<ScaffoldShell> createState() => _ScaffoldShellState();
+}
+
+class _ScaffoldShellState extends State<ScaffoldShell> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: navigationShell,
+      body: SafeArea(child: widget.navigationShell),
       bottomNavigationBar: NavigationBar(
-        selectedIndex: navigationShell.currentIndex,
+        selectedIndex: widget.navigationShell.currentIndex,
         labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
         destinations: [
           NavigationDestination(icon: Icon(Icons.map), label: 'Map'),
@@ -26,8 +32,21 @@ class ScaffoldShell extends StatelessWidget {
     );
   }
 
+  Widget? _buildBackButton(BuildContext context) {
+    if (Navigator.of(context).canPop()) {
+      return IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      );
+    } else {
+      return null; // No back button if we can't pop the stack
+    }
+  }
+
   void _goBranch(int index) {
-    navigationShell.goBranch(index,
-        initialLocation: index == navigationShell.currentIndex);
+    widget.navigationShell.goBranch(index,
+        initialLocation: index == widget.navigationShell.currentIndex);
   }
 }
