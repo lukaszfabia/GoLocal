1bfd20b4e40d79a2fc1ca58e0a58b9c930b5d5e2
jantar <mikikubs@gmail.com>
2025-01-02 20:25:24 +0100

    feat: moved preference survey answer creation to factory

--- mobile-app/lib/src/preference_survey/domain/preference_survey_answer.dart
+++ mobile-app/lib/src/preference_survey/domain/preference_survey_answer.dart
@@ -15,6 +15,36 @@ class PreferenceSurveyAnswer {
     this.options,
   });
 
+  factory PreferenceSurveyAnswer.fromJson(
+      int preferenceSurveyId, int questionId, int userId, String value) {
+    if (value == 'true' || value == 'false') {
+      return PreferenceSurveyAnswer(
+        surveyId: preferenceSurveyId,
+        questionId: questionId,
+        userId: userId,
+        toggle: value == 'true',
+      );
+    } else if (value.contains(',')) {
+      final options = value
+          .split(',')
+          .map((e) => {'OptionID': int.parse(e.trim())})
+          .toList();
+      return PreferenceSurveyAnswer(
+        surveyId: preferenceSurveyId,
+        questionId: questionId,
+        userId: userId,
+        options: options,
+      );
+    } else {
+      return PreferenceSurveyAnswer(
+        surveyId: preferenceSurveyId,
+        questionId: questionId,
+        userId: userId,
+        optionId: int.tryParse(value),
+      );
+    }
+  }
+
   Map<String, dynamic> toJson() {
     return {
       'SurveyID': surveyId,
