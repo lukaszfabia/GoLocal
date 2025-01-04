0c713b1d21da9539fc298379c57d91c0864e3c68
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-01-05 00:19:44 +0100

    fixed conflict

--- mobile-app/lib/src/routing/router.dart
+++ mobile-app/lib/src/routing/router.dart
@@ -6,7 +6,7 @@ import 'package:golocal/src/auth/ui/auth_screen.dart';
 import 'package:golocal/src/event/domain/event.dart';
 import 'package:golocal/src/event/manage/event_create_page.dart';
 import 'package:golocal/src/event/report/report_event_page.dart';
-import 'package:golocal/src/event/ui/event_detail.dart';
+import 'package:golocal/src/event/ui/event_detail_page.dart';
 import 'package:golocal/src/event/ui/events_map.dart';
 import 'package:golocal/src/event/ui/events_view_page.dart';
 import 'package:golocal/src/preference_survey/ui/preference_survey_page.dart';
@@ -97,7 +97,7 @@ abstract class AppRouter {
                       path: AppRoute.eventDetail.path,
                       builder: (context, state) {
                         final event = state.extra as Event;
-                        return EventDetail(event: event);
+                        return EventDetailPage(event: event);
                       },
                       routes: [
                         GoRoute(
