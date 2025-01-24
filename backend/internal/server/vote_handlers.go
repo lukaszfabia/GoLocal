d2b19324ab5099b250943e34acb224b1dfca8b08
lukaszfabia <ufabia03@gmail.com>
2025-01-24 21:40:34 +0100

    feat: vote function

--- backend/internal/server/vote_handlers.go
+++ backend/internal/server/vote_handlers.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"backend/internal/forms"
 	"backend/internal/models"
 	"backend/pkg/parsers"
 	"log"
@@ -14,11 +15,34 @@ func (s *Server) VoteHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		s.getVote(w, r)
+	case http.MethodPost:
+		s.vote(w, r)
 	default:
 		s.NewResponse(w, http.StatusBadRequest, nil)
 		return
 	}
 
+}
+
+func (s *Server) vote(w http.ResponseWriter, r *http.Request) {
+	form, err := parsers.DecodeJSON[forms.VoteForm](r)
+
+	if err != nil {
+		s.InvalidFormResponse(w)
+	}
+
+	user, ok := r.Context().Value("user").(*models.User)
+	if !ok {
+		s.NewResponse(w, http.StatusUnauthorized, "Unauthorized access")
+		return
+	}
+
+	if err := s.db.VoteService().Vote(*form, *user); err != nil {
+		s.NewResponse(w, http.StatusBadRequest, err)
+		return
+	}
+
+	s.NewResponse(w, http.StatusOK, "Successfully changed")
 }
 
 func (s *Server) getVote(w http.ResponseWriter, r *http.Request) {
