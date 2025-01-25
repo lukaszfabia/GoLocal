ff8b634dcb5f7747e92d9746aa8c2b1e22a05b1e
jantar <mikikubs@gmail.com>
2025-01-25 23:02:00 +0100

    feat: test firebase notification working on frontend

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:golocal/src/app.dart';
+import 'package:golocal/src/notifications_service/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await NotificationService.instance.init();
   runApp(GoLocalApp());
 }
