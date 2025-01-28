6ac9c9598b031e674fea3f5c38eca9f203604894
jantar <mikikubs@gmail.com>
2025-01-28 11:37:23 +0100

    docs: added docs for preference survey and vote, specified in readme how to use it

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,6 @@
+///@nodoc
+library;
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:golocal/src/app.dart';
---
+++ frontend/lib/preference_survey.dart
@@ -0,0 +1,1 @@
+export 'src/preference_survey/bloc/preference_survey_bloc.dart';
